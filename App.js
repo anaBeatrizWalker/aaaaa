f790e822b56e2ffd322fe1ccb654b3f0eebecbc5
anaBeatrizWalker <anabiawalker@gmail.com>
2023-04-12 07:25:31 -0300

    Alterando aplicação para componente funcional

--- App.js
+++ App.js
@@ -1,47 +1,36 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, Button, Image } from 'react-native';
 
-export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      n1: '',
-      n2: '',
-      resultado: 0
-    }
-    this.multiplicar = this.multiplicar.bind(this);
-    this.limparCampos = this.limparCampos.bind(this);
+export default function App() {
+  const [n1, setN1] = useState('');
+  const [n2, setN2] = useState('');
+  const [resultado, setResultado] = useState(0);
+
+  function multiplicar(){
+    setResultado(n1 * n2);
   }
 
-  multiplicar(){
-    this.setState({
-      resultado: this.state.n1 * this.state.n2
-    })
+  function limparCampos(){
+    setN1('');
+    setN2('');
+    setResultado(0);
   }
 
-  limparCampos(){
-    this.setState({
-      n1: '',
-      n2: '',
-      resultado: 0
-    })
-  }
+  return (
+    <View style={{alignItems: 'center', marginTop: 25}}>
+      <Text  style={{fontWeight: 'bold', fontSize: 20}}>Multiplicação de Dois Números</Text>
 
-  render(){
-    return (
-      <View style={{alignItems: 'center', marginTop: 25}}>
-        <Text  style={{fontWeight: 'bold', fontSize: 20}}>Multiplicação de Dois Números</Text>
+      <Image style={{width: 150, height: 150, margin: 20}} source={require('./img/multiplicacao.png')}></Image>
 
-        <TextInput style={{marginTop: 10, width: 200, padding: 10, backgroundColor: '#DCDCDC', borderRadius: 5}} placeholder="Digite um número: " onChangeText={(n) => this.setState({n1: n})} value={this.state.n1}></TextInput>
+      <TextInput style={{marginTop: 10, width: 200, padding: 10, backgroundColor: '#DCDCDC', borderRadius: 5}} placeholder="Digite um número: " onChangeText={(n) => setN1(n)} value={n1}></TextInput>
 
-        <TextInput style={{margin: 10, width: 200, padding: 10, backgroundColor: '#DCDCDC', borderRadius: 5}}  placeholder="Digite mais um número: " onChangeText={(n) => this.setState({n2: n})} value={this.state.n2}></TextInput>
+      <TextInput style={{margin: 10, width: 200, padding: 10, backgroundColor: '#DCDCDC', borderRadius: 5}}  placeholder="Digite mais um número: " onChangeText={(n) => setN2(n)} value={n2}></TextInput>
 
-        <Button title='Calcular' onPress={this.multiplicar}></Button>
+      <Button title='Calcular' onPress={multiplicar}></Button>
 
-        <Button title='Limpar' onPress={this.limparCampos}></Button>
+      <Button title='Limpar' onPress={limparCampos}></Button>
 
-        <Text style={{margin: 15, fontWeight: 'bold', fontSize: 24}}>Resultado: {this.state.resultado}</Text>
-      </View>
-    );
-  }
+      <Text style={{margin: 15, fontWeight: 'bold', fontSize: 24}}>Resultado: {resultado}</Text>
+    </View>
+  );
 }
